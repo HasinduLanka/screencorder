35f871e71fc8dfd40315baecb6e1dde27d762063
HasinduLanka <hlrathnayaka@gmail.com>
2021-05-20 15:57:28 +0530

    build and commit

--- build.sh
+++ build.sh
@@ -1,13 +1,58 @@
 #!/bin/bash
 
-rm -rf build
-mkdir build
+echo Commit $1
+
+echo "Cleaning up"
+
+rm -rf ../build
+mkdir -p ../build/www
+
+echo "Scanning static assests"
+
+cp -r js ../build/www/
+cp -ra .git ../build/www/
+cp .gitignore ../build/www/
+cp 404.html ../build/www/
+cp favicon.ico ../build/www/
+cp index.html ../build/www/
+cp manifest.json ../build/www/
+cp screen.png ../build/www/
+cp serviceworker.js ../build/www/
+
+echo "Copying static assests"
+
+cp -ra ../build/www ../build/linux.x86
+cp -ra ../build/www ../build/linux.amd64
+cp -ra ../build/www ../build/linux.arm
+cp -ra ../build/www ../build/linux.arm64
 
 echo "Building for Linux"
 
-env GOOS=linux GOARCH=386 go build -o build/linux.x86/m3udownloader .
-env GOOS=linux GOARCH=amd64 go build -o build/linux.amd64/m3udownloader .
-env GOOS=linux GOARCH=arm go build -o build/linux.arm/m3udownloader .
-env GOOS=linux GOARCH=arm64 go build -o build/linux.arm64/m3udownloader .
+env GOOS=linux GOARCH=386 go build -o ../build/linux.x86/m3udownloader .
+env GOOS=linux GOARCH=amd64 go build -o ../build/linux.amd64/m3udownloader .
+env GOOS=linux GOARCH=arm go build -o ../build/linux.arm/m3udownloader .
+env GOOS=linux GOARCH=arm64 go build -o ../build/linux.arm64/m3udownloader .
+
+echo "Configuring GITs"
+
+cd ../build/linux.x86
+git branch -m linux.x86
+git add .
+git commit -m "build-$1"
+
+cd ../linux.amd64
+git branch -m linux.amd64
+git add .
+git commit -m "build-$1"
+
+cd ../linux.arm
+git branch -m linux.arm
+git add .
+git commit -m "build-$1"
+
+cd ../linux.arm64
+git branch -m linux.arm64
+git add .
+git commit -m "build-$1"
 
 echo "Done"

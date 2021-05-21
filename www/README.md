fe076e4ecf8522aa5d2649399c72eedf67b16868
HasinduLanka <hlrathnayaka@gmail.com>
2021-05-21 14:53:46 +0530

    build-

---
+++ www/README.md
@@ -0,0 +1,117 @@
+# Screencorder
+## Screen recorder for linux on Wayland and X11 
+### Simple, fast and open-source with GUI 
+
+
+# Installation
+
+## 1. Make sure you have required dependencies 
+`pulseaudio-utils lame mpg123 ffmpeg`
+```
+# Example : On debian/ubuntu
+sudo apt-get install pulseaudio-utils lame mpg123 ffmpeg
+```
+
+
+## 2. Download
+You can also `git clone` it for easy updates. 
+`Total Size ~6 MB` 
+
+Download the first one (x64) if you are not sure.
+
+| Platform | Download zip | git clone |
+| :--------: | :---------------: | :--------------- |
+| **x64** | [**Download**](https://github.com/HasinduLanka/screencorder/archive/refs/heads/linux.amd64.zip) | `git clone --depth=1 --branch=linux.amd64 --single-branch https://github.com/HasinduLanka/screencorder` |
+| x86 | [Download](https://github.com/HasinduLanka/screencorder/archive/refs/heads/linux.x86.zip) | `git clone --depth=1 --branch=linux.x86 --single-branch https://github.com/HasinduLanka/screencorder` |
+| ARM64 | [Download](https://github.com/HasinduLanka/screencorder/archive/refs/heads/linux.arm64.zip) | `git clone --depth=1 --branch=linux.arm64 --single-branch https://github.com/HasinduLanka/screencorder` |
+| ARM | [Download](https://github.com/HasinduLanka/screencorder/archive/refs/heads/linux.arm.zip) | `git clone --depth=1 --branch=linux.arm --single-branch https://github.com/HasinduLanka/screencorder` |
+
+## 3. Unzip it
+ where you want to store recordings
+## 4. Run `screencorder`
+Example:  `./screencorder`
+
+A browser will open. If not, open a browser and go to http://localhost:49542/
+
+While screencorder *should* work on all major browsers, we recommend to use a chromium based browser like **Brave or Google Chrome**.
+
+## 5. Select a recording quality
+Default is original
+
+| Quality | Bitrate | FPS |
+| :-------: | :-------: | :---: |
+| Original | Window bitrate | Screen Refresh rate |
+| Full HD | 8 Mbps | 60 |
+| HD | 5 Mbps | 30 |
+| Medium | 2.5 Mbps | 30 |
+| Low | 1 Mbps | 24 |
+| Lowest | 0.6 Mbps | 16 |
+| Text Optimized | Window bitrate | 10 |
+
+
+These can vary according to your screen configs. File size depends on what you are recording.
+
+Audio will be recorded @ `128 kbps`
+
+## 6. Click `Start and Record`
+Share your window or desktop. 
+
+On **wayland** you will only be able to share windows. Not desktops.
+
+## 7. Click `Stop Sharing` button
+ from browser toolbar or the floating menu. It may be minimized.
+
+## 8. See your recordings from `workspace` folder 
+inside current folder. You can safely stop `screencorder` now
+
+
+![visitors](https://visitor-badge.glitch.me/badge?page_id=hasindulanka.screencorder)
+
+```
+⭐⭐⭐ Start this repo up there ⭐⭐⭐
+```
+
+# How this screen recorder works on wayland
+Most screen recorders out there do not work on wayland. Why?
+Because wayland is much secure than old X11. Wayland doesn't let every program to see what others doing. It's a bit harder to gain access.
+
+But web browsers, like Chromium have handled these security features properly. That's why we use a *local Progressive Web App* as the frontend and a backend written in Go. The **PWA records the screen** and sends them to the backend as blobs. **Backend records system sounds** and combines all together in realtime.
+
+
+# Contributions
+Join us with code. Any new features and changes are welcome!
+This is a small project. You can get your head around easily. 
+
+# Copyleft
+This project has been **copyleft** by Hasindu Lanka and Harindu Wijesinghe of **Bitblazers**, Sri Lanka.
+
+What that means?
+It means you can use this for any non-commercial, commercial applience ; copy, distribute and rebrand for free. But when you copy, distribute or rebrand, that product must also be free, **opensource** and **copyleft**.
+
+Just be kind to inform us what great thing you made using this. We won't ask for money :-) 
+
+
+
+# --------------------------------------------
+# Possible Search querries
+
+# Screen recorder for wayland
+# Screen recorder for x11
+# Screen recorder for linux
+# Screen recorder for kde
+# Screen recorder for gnome
+# Screen recorder for ubuntu
+# Screen recorder for kubuntu
+# record screen 21.04
+# record screen wayland
+# sreen cast linux
+# sreen cast wayland
+# record window linux
+# my screen recorder not working on wayland
+# my screen recorder not working after 21.04 update
+# screen record black, blank
+# vokoscreen not working crashes
+# simplescreenrecorder not working
+# download screen window cast recorder for wayland ubuntu debain
+
+# --------------------------------------------
---
+++ www/js/main.js
@@ -0,0 +1,352 @@
+/*
+ *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+'use strict';
+
+// Polyfill in Firefox.
+// See https://blog.mozilla.org/webrtc/getdisplaymedia-now-available-in-adapter-js/
+if (adapter.browserDetails.browser == 'firefox') {
+  adapter.browserShim.shimGetDisplayMedia(window, 'screen');
+}
+
+const video = document.querySelector('video');
+var mediaRecorder;
+var RecordEnabled = true;
+var QualityOptions = { mimeType: 'video/webm' }
+var GUMConstraints = { video: true };
+
+var filename = "Record-" + Date.now().toString();
+var fileindex = 1;
+var finalFiles = [];
+var IsRecording = false;
+
+GetURL("handshake/" + filename)
+
+const DivSetQlt = document.getElementById('DivSetQlt');
+
+
+
+function handleError(error) {
+  errorMsg(`getDisplayMedia error: ${error.name}`, error);
+}
+
+function errorMsg(msg, error) {
+  const errorElement = document.querySelector('#errorMsg');
+  errorElement.innerHTML += `<p>${msg}</p>`;
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
+
+
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', () => {
+  RecordEnabled = false;
+  navigator.mediaDevices.getDisplayMedia(GUMConstraints)
+    .then(handleSuccess, handleError);
+});
+const startRecButton = document.getElementById('startRecButton');
+startRecButton.addEventListener('click', () => {
+  RecordEnabled = true;
+  navigator.mediaDevices.getDisplayMedia(GUMConstraints)
+    .then(handleSuccess, handleError);
+});
+
+
+if ((navigator.mediaDevices && 'getDisplayMedia' in navigator.mediaDevices)) {
+  startButton.disabled = false;
+  startRecButton.disabled = false;
+  DivSetQlt.hidden = false;
+} else {
+  errorMsg('getDisplayMedia is not supported');
+}
+
+
+function handleSuccess(stream) {
+  startButton.disabled = true;
+  startRecButton.disabled = true;
+  DivSetQlt.hidden = true;
+  video.srcObject = stream;
+
+  // demonstrates how to detect that the user has stopped
+  // sharing the screen via the browser UI.
+  stream.getVideoTracks()[0].addEventListener('ended', () => {
+    errorMsg('The user has ended sharing the screen');
+    IsRecording = false;
+
+    startButton.disabled = false;
+    startRecButton.disabled = false;
+    DivSetQlt.hidden = false;
+
+  });
+
+  if (RecordEnabled) {
+    console.log(stream);
+    GetURL("start/" + filename)
+    // var options = { mimeType: "video/webm; codecs=vp8" };
+    IsRecording = true;
+    mediaRecorder = new MediaRecorder(stream, QualityOptions);
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+
+
+
+    setTimeout(event => {
+      RestartMediaRecorder()
+    }, 10000);
+  }
+}
+
+
+function RestartMediaRecorder() {
+  console.log("Chunk done");
+  if (IsRecording) {
+    mediaRecorder.stop();
+    mediaRecorder.start();
+    setTimeout(event => {
+      RestartMediaRecorder()
+    }, 20000);
+  }
+
+}
+
+var recordedChunks = [];
+
+
+
+async function downloadText(txt, filename) {
+  var blob = new Blob([txt], { type: 'text/plain' });
+
+  // this will create a link tag on the fly
+  // <a href="..." download>
+  var link = document.createElement('a');
+  link.setAttribute('href', URL.createObjectURL(blob));
+  link.setAttribute('download', filename);
+
+  // NOTE: We need to add temporarily the link to the DOM so
+  //       we can trigger a 'click' on it.
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function handleDataAvailable(event) {
+  console.log("data-available");
+  if (event.data.size > 0) {
+    recordedChunks.push(event.data);
+    console.log(recordedChunks);
+    // download();
+    uploadRecorded();
+    if (fileindex >= 5 || !IsRecording) {
+      FinalizeRecord()
+      if (IsRecording) {
+        GetURL("start/" + filename)
+      }
+    }
+  } else {
+    // ...
+  }
+
+  if (!IsRecording) {
+    EndRecord()
+  }
+
+
+}
+
+
+function FinalizeRecord() {
+
+  finalFiles.push(filename);
+
+  var fflist = "";
+
+  for (let i = 1; i < fileindex; i++) {
+    fflist = fflist + "file '" + filename + "-" + pad(i) + ".webm'\n";
+  }
+
+  uploadText("final/" + filename, fflist)
+
+  filename = "Record-" + Date.now().toString()
+  fileindex = 1
+}
+
+
+function EndRecord() {
+
+  var fflist = "";
+
+  finalFiles.forEach(f => {
+    fflist = fflist + "file '" + f + ".webm'\n";
+  });
+
+  uploadText("end/" + filename, fflist)
+
+  filename = "Record-" + Date.now().toString()
+  fileindex = 1
+  finalFiles = [];
+}
+
+function download() {
+  var blob = new Blob(recordedChunks, {
+    type: "video/webm"
+  });
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = filename + "-" + pad(fileindex) + ".webm";
+  fileindex += 1;
+  a.click();
+  window.URL.revokeObjectURL(url);
+  recordedChunks = []
+}
+
+
+function uploadRecorded() {
+  var blob = new Blob(recordedChunks, {
+    type: "video/webm"
+  });
+  uploadBlob(`chunk/` + filename + "-" + pad(fileindex), blob)
+  fileindex += 1;
+
+  recordedChunks = []
+}
+async function uploadText(path, txt) {
+  var blob = new Blob([txt], { type: 'text/plain' });
+  uploadBlob(path, blob);
+}
+
+//http://localhost:49542
+async function uploadBlob(path, blob) {
+  fetch(`/` + path, { method: "POST", body: blob, mode: 'no-cors' })
+    .then(response => response.text().then(value => console.log(value)))
+}
+
+function pad(num) {
+  var s = "000000000" + num;
+  return s.substr(s.length - 10);
+}
+
+//http://localhost:49542
+async function GetURL(path) {
+  fetch(`/` + path, { method: "GET", mode: 'no-cors' })
+    .then(response => response.text().then(value => console.log(value)))
+}
+
+
+
+function SetQlt(q) {
+
+  switch (q) {
+    case 0:
+      QualityOptions = {
+        mimeType: 'video/webm'
+      }
+      GUMConstraints = { video: true };
+      break;
+
+    case 1:
+      QualityOptions = {
+        videoBitsPerSecond: 8000000,
+        mimeType: 'video/webm'
+      }
+      GUMConstraints = { video: { frameRate: { ideal: 60, max: 60 } } };
+      break;
+
+    case 2:
+      QualityOptions = {
+        videoBitsPerSecond: 5000000,
+        mimeType: 'video/webm'
+      }
+      GUMConstraints = { video: { frameRate: { ideal: 30, max: 30 } } };
+      break;
+
+    case 3:
+      QualityOptions = {
+        videoBitsPerSecond: 2500000,
+        mimeType: 'video/webm'
+      }
+      GUMConstraints = { video: { frameRate: { ideal: 30, max: 30 } } };
+      break;
+
+    case 4:
+      QualityOptions = {
+        videoBitsPerSecond: 1000000,
+        mimeType: 'video/webm'
+      }
+      GUMConstraints = { video: { frameRate: { ideal: 24, max: 30 } } };
+      break;
+
+    case 5:
+      QualityOptions = {
+        videoBitsPerSecond: 600000,
+        mimeType: 'video/webm'
+      }
+      GUMConstraints = { video: { frameRate: { ideal: 16, max: 24 } } };
+      break;
+    case 100:
+      QualityOptions = {
+        mimeType: 'video/webm'
+      }
+      GUMConstraints = { video: { frameRate: { ideal: 10, max: 20 } } };
+      break;
+    default:
+      break;
+  }
+}
+
+
+
+function PlaybackMic() {
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+  var aCtx;
+  var analyser;
+  var microphone;
+  if (navigator.getUserMedia) {
+    navigator.getUserMedia(
+      { audio: true },
+      function (stream) {
+
+        aCtx = new (window.AudioContext || window.webkitAudioContext)();
+        // aCtx = new AudioContext();
+
+        window.AudioContext.
+          microphone = aCtx.createMediaStreamSource(stream);
+        var destination = aCtx.destination;
+        microphone.connect(destination);
+
+
+      },
+      function () { console.log("Error 003.") }
+    );
+  }
+}
+
+// const downloadButton = document.getElementById('downloadButton');
+
+// downloadButton.addEventListener('click', () => {
+
+//   var fflist = "";
+//   var fflistfilename = filename + ".fflist";
+
+//   var cmd = "#!/bin/bash\n";
+//   cmd = cmd + "ffmpeg -f concat -safe 0 -i " + fflistfilename + " -c copy " + filename + ".webm\nrm -f " + filename + "-*.webm\nrm -f " + filename + ".sh\n";
+
+
+//   for (let i = 1; i < fileindex; i++) {
+//     fflist = fflist + "file '" + filename + "-" + pad(i) + ".webm'\n";
+//   }
+
+//   downloadText(fflist, fflistfilename);
+//   // uploadText("final/" + filename, fflist)
+//   downloadText(cmd, filename + ".sh");
+
+// });
